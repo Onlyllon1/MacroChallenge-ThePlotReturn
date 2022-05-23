6a1a90f178aef9e06154af7e3d1bd755e5cbed14
Onlyllon1 <francescoliviero15@gmail.com>
2022-05-23 12:44:15 +0200

    Secondo gIT

--- MedWaste/MedWaste/ContentView.swift
+++ MedWaste/MedWaste/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Primo Git!")
-            .padding()
+       VStack
+        {
+            Text("Primo Git!")
+            Text("SecondoGit")
+            .padding()}
     }
 }
 
