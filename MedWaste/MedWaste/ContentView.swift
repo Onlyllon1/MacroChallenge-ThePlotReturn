8cd780e72106d0c297e37b40ce24335451b255e1
Onlyllon1 <francescoliviero15@gmail.com>
2022-05-23 12:40:41 +0200

    First GitHub

---
+++ MedWaste/MedWaste/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MedWaste
+//
+//  Created by Francesco on 23/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Primo Git!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MedWaste/MedWaste/MedWasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedWasteApp.swift
+//  MedWaste
+//
+//  Created by Francesco on 23/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MedWasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
