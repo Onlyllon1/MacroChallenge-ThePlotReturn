968983c338834121d925e4bdc8d2e6c3f0c256ca
Irene Fernando <irene87fernando@gmail.com>
2022-05-29 17:57:02 +0200

    SearchBar

--- MedWaste/MedWaste/Views/Main/CabinetView.swift
+++ MedWaste/MedWaste/Views/Main/CabinetView.swift
@@ -9,8 +9,30 @@
 import SwiftUI
 
 struct CabinetView: View {
+    @State public var searchQuery = ""
+    let data = (1...10).map { "Item \($0)" }
+       let columns = [
+              GridItem(.flexible(minimum: 90)),
+              GridItem(.flexible(minimum: 90))
+          ]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+           
+              
+               VStack{
+                   Text("Search for")
+                                   .searchable(text: $searchQuery, prompt: "Search for medicines")
+               ScrollView {
+                           LazyVGrid(columns: columns, spacing: 20) {
+                               ForEach(data, id: \.self) { item in
+                                   MedCardView()
+                                       .padding(5)
+                               }
+                           }
+               }
+       //                    .padding(.horizontal)
+               }.searchable(text: $searchQuery)
+    }
     }
 }
 
