ef773f784bacecd4f8d9c2f8eff7f16733a77fd7
Irene Fernando <irene87fernando@gmail.com>
2022-05-30 09:58:09 +0200

    Minor

--- MedWaste/MedWaste/Views/Main/ContentView.swift
+++ MedWaste/MedWaste/Views/Main/ContentView.swift
@@ -10,53 +10,20 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var tabSelection: Tabs = .tab1
-    @State public var tex = "d"
-    
-    @State private var showScanner = false
-    @State private var showMap = false
-    @State private var isRecognizing = false
-    
-    
-    
-    enum Tabs{ case tab1, tab2, tab3 }
-    
-    func returnNaviBarTitle(tabSelection: Tabs) -> String{
-        switch tabSelection{
-            case .tab1: return "Riepilogo"
-            case .tab2: return "Armadietto"
-            case .tab3: return "Statistiche"
-        }
-    }
-    
+
     var body: some View {
-//        NavigationView{
-            TabView(selection: $tabSelection){
+            TabView(){
                 SummaryView()
                     .tabItem {Label("Riepilogo", systemImage: "square.grid.2x2.fill")}
-//                    .tag(Tabs.tab1)
+
                 CabinetView()
                     .tabItem {Label("Armadietto", systemImage: "heart.text.square.fill")}
-//                    .tag(Tabs.tab2)
+
                 StatisticsView()
                     .tabItem {Label("Statistiche", systemImage: "chart.pie.fill")}
-//                    .tag(Tabs.tab3)
-            }
-//            .navigationBarHidden(true)
-//            .navigationBarTitle("")
-//                    .navigationBarHidden(true)
-//            .navigationBarTitle(returnNaviBarTitle(tabSelection: self.tabSelection))
-//            .navigationBarItems(trailing:
-//                                    HStack(spacing: 20){
-//                Button(action: { showMap = true }, label: { Image(systemName: "map.circle.fill").scaleEffect(1.5)})
-//                Button(action: {showScanner = true }, label: { Image(systemName: "plus.circle.fill").foregroundColor(CustomColor.darkblue).scaleEffect(1.5)})
-//            }
-//            )
 
-            
-//        }
-//        .sheet(isPresented: $showMap, content: {MapView()})
-//            .sheet(isPresented: $showScanner, content: {NewItemView()})
+            }
+
     }
 }
 
